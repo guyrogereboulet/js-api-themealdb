4b761b540bfe8f75189bbefd793b9febb07c796d
Guy Roger Eboulet <eboulet87@gmail.com>
2020-11-03 11:36:20 +0100

    randomSearch

--- script.js
+++ script.js
@@ -7,19 +7,24 @@ let search = '';
 
 
 //Facciamo una funzione asincrona con il metodo fetch per ottenere il JSON
-const fetchSearch = async() => {
+const fetchSearch = async(url) => {
     meals = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+        // `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+
+        //per fare una chiamata sia sul "search" che sul "random" creiamo un oarametro "url" che sarà usato per entrambe le chiamate
+        `https://www.themealdb.com/api/json/v1/1/${url}`)
         .then(res => res.json())
         .then(res => res.meals)
         // .then(res => console.log(res))
 
-        console.log(meals);
+        // console.log(meals);
 }
+
+//SEARCH MEAL
 
 //Mostriamo i risultati della ricerca a video
 const searchDisplay = async() => {
-    await fetchSearch();
+    await fetchSearch(search);
 
     if (meals == null) {
         results.innerHTML = '<span class="noResult">Nessun risultato </span>'
@@ -48,11 +53,41 @@ const searchDisplay = async() => {
 //Prendiamo il valore dell'input 
 searchInput.addEventListener('input', (e) => {
     // console.log(e.target.value);
-    search = e.target.value;
+    search = `search.php?s=${e.target.value}`  
     searchDisplay();
 })
 
+// RANDOM MEAL
+
+const randomMealDisplay = async() => {
+    await fetchSearch('random.php');
 
 
+    results.innerHTML = (
 
-fetchSearch();
+            meals.map( meal => (
+
+            `
+                <div class="randomContainer">
+                <h2>${meal.strMeal}</h2>
+                <div class="infos">
+                    <div>origin : ${meal.strArea}</div>
+                    <div>catégory : ${meal.strCategory}</div>
+                </div>
+                <img src='${meal.strMealThumb}' />
+                <p>${meal.strInstructions}</p>
+                <a href="${meal.strYoutube}" target="_blank"><i class="fab fa-youtube"></i></a>
+                </div>
+        `
+        ))
+
+
+    );
+
+    
+};
+
+
+randomMeal.addEventListener('click', randomMealDisplay);
+
+randomMealDisplay();
