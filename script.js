3e701042aec27ec3486322b4ad63a702770785cf
Guy Roger Eboulet <eboulet87@gmail.com>
2020-11-03 10:01:25 +0100

    searchDisplay

--- script.js
+++ script.js
@@ -3,15 +3,32 @@
 const searchInput = document.getElementById('searchInput');
 const randomMeal = document.getElementById('randomMeal');
 const results = document.getElementById('results');
+let search = '';
 
 
-//Facciamo una funzione asincrona
+//Facciamo una funzione asincrona con il metodo fetch per ottenere il JSON
 const fetchSearch = async() => {
     meals = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`)
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=tomato`)
         .then(res => res.json())
-        .then(res => console.log(res))
+        .then(res => res.meals)
+        // .then(res => console.log(res))
+
+        console.log(meals);
 }
+
+//Mostriamo i risultati della ricerca a video
+
+const serchDisplay = async() => {
+    await fetchSearch();
+};
+
+searchInput.addEventListener('input', (e) => {
+    console.log(e.target.value);
+    search = e.target.value;
+})
+
+
 
 
 fetchSearch();
